1a6a01f10a880c648230f8d1cba1c1485be736e3
bulentbariskilic@gmail.com <bulentbariskilic@gmail.com>
2021-05-09 23:49:55 +0300

    Initial commit

---
+++ src/test/java/com/bbkdevelopment/hrms/HrmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bbkdevelopment.hrms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
