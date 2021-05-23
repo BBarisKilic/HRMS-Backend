b06328a03f51c234d772965afb22076918fc6ea2
bulentbariskilic@gmail.com <bulentbariskilic@gmail.com>
2021-05-23 14:30:12 +0300

    Created Result object to handle API requests.

---
+++ src/main/java/com/bbkdevelopment/hrms/core/utilities/results/Result.java
@@ -0,0 +1,23 @@
+package com.bbkdevelopment.hrms.core.utilities.results;
+
+public class Result {
+    private boolean success;
+    private String message;
+
+    public Result(boolean success) {
+        this.success = success;
+    }
+
+    public Result(boolean success, String message) {
+        this(success);
+        this.message = message;
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
---
+++ src/main/java/com/bbkdevelopment/hrms/core/utilities/results/SuccessResult.java
@@ -0,0 +1,11 @@
+package com.bbkdevelopment.hrms.core.utilities.results;
+
+public class SuccessResult extends Result{
+    public SuccessResult() {
+        super(true);
+    }
+
+    public SuccessResult(String message) {
+        super(true, message);
+    }
+}
