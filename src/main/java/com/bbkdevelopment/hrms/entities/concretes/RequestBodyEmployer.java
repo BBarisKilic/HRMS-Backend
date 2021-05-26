a9cd1fb579564922573073adc61b57f0ef1b1aa0
bulentbariskilic@gmail.com <bulentbariskilic@gmail.com>
2021-05-26 11:43:20 +0300

    Created RequestBodyEmployer class.

---
+++ src/main/java/com/bbkdevelopment/hrms/entities/concretes/RequestBodyEmployer.java
@@ -0,0 +1,17 @@
+package com.bbkdevelopment.hrms.entities.concretes;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+
+@Data
+@Entity
+@EqualsAndHashCode(callSuper = true)
+public class RequestBodyEmployer extends Employer {
+    @NotEmpty
+    @NotBlank(message = "Second password can not be empty!")
+    private String secondPassword;
+}
