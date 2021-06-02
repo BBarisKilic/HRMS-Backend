52c0eda1efce470ac5d02132d952b580006a2dfa
bulentbariskilic@gmail.com <bulentbariskilic@gmail.com>
2021-06-02 20:34:03 +0300

    Country entity added to project.

---
+++ src/main/java/com/bbkdevelopment/hrms/entities/concretes/Country.java
@@ -0,0 +1,27 @@
+package com.bbkdevelopment.hrms.entities.concretes;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+
+@Data
+@Entity
+@Table(name = "countries")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Country {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "title")
+    @NotEmpty
+    @NotBlank(message = "Country name can not be empty!")
+    private String country_name;
+}
